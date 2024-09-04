6ef915c767ec94386ae187cff4f9c41ad1dde575
l3lackcurtains <9688803+l3lackcurtains@users.noreply.github.com>
2024-09-03 17:43:00 -0700

    Deployed new contract for test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 #### Test contracts in holesky
 
 - **Token**: [0x9Ee1d6a13B1724803CF7EE60014E0cF9EC4b5052](https://holesky.etherscan.io/address/0x9Ee1d6a13B1724803CF7EE60014E0cF9EC4b5052)
-- **Staking**: [0xb90926B78b236D6dc2DCbA64e5Fb7ED9e114D943](https://holesky.etherscan.io/address/0xb90926B78b236D6dc2DCbA64e5Fb7ED9e114D943)
+- **Staking**: [0x451a9A54aAD00aABbCCA220C15616B904B6a921D](https://holesky.etherscan.io/address/0x451a9A54aAD00aABbCCA220C15616B904B6a921D)
--- scripts/staking.ts
+++ scripts/staking.ts
@@ -1,14 +1,16 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 
 
 async function main() {
 
 
     const tokenAddress = "0x9Ee1d6a13B1724803CF7EE60014E0cF9EC4b5052";
-    const merkleRoot = "0xf0b1851d6151590186347cfd2e204cb37a3a3c13b125dacf5eaa1192d6de05d4";
+    const managerAddress = "0x57291FE9b6dC5bBeF1451c4789d4e578ce956219";
 
     const stakingContract = await ethers.getContractFactory("Staking");
-    const staking = await stakingContract.deploy(tokenAddress, merkleRoot);
+    const staking = await upgrades.deployProxy(stakingContract, [tokenAddress, managerAddress], {
+        kind: "uups"
+    });
     await staking.waitForDeployment();
     console.log("Staking contract deployed to:", await staking.getAddress());
 }
